531ea74b43eb5d6b80945cf8562d041c057bb942
Claudio Afonso <claudio.afonso@avaloncx.com>
2025-09-02 20:03:02 +0200

    Fix PostHogProvider import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
-import PostHogProvider from "../components/PostHogProvider";
+import PostHogProvider from "@/components/PostHogProvider";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
